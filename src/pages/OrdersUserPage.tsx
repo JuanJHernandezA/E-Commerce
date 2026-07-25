438d41eee410a08fa61dd8a8bcf788bc2d4a6045
Juan José Hernández <juan.hernandez.arenas@correounivalle.edu.co>
2026-07-25 16:38:32 -0500

    Corregida página de pedidos si no hay pedidos

--- src/pages/OrdersUserPage.tsx
+++ src/pages/OrdersUserPage.tsx
@@ -5,22 +5,23 @@ import { useOrders } from '../hooks';
 
 export const OrdersUserPage = () => {
   const {data:orders,isLoading} =useOrders();
-  if(isLoading || !orders) return <Loader />
+  if(isLoading) return <Loader />
   return (
     <div className='flex flex-col gap-6 items-center'>
       <div className="flex gap-2">
         <h1 className="text-3xl font-bold">
           Pedidos
         </h1>
-        <span className="w-5 h-5 rounded-full bg-black text-white text-[11px] flex justify-center items-center mt-1">
-          {orders.length}
-        </span>
+        {orders && (<span className="w-5 h-5 rounded-full bg-black text-white text-[11px] flex justify-center items-center mt-1">
+          {orders?.length}
+        </span>)}
+        
       </div>
 
-      {orders.length===0?(<>
+      {!orders ?(<>
       <p className="text-slate-600 text-[13px]">
         Todavía no has hecho ningún pedido</p>
-        <Link to='/celulares' className='bg-black text-white uppercase font-semibol tracking-widest text-xs py-4 rounded-full px-8 cursor-pointer'>Empezar a comprar</Link>
+        <Link to='/productos' className='bg-black text-white uppercase font-semibol tracking-widest text-xs py-4 rounded-full px-8 cursor-pointer'>Empezar a comprar</Link>
         </>):(<TableOrders orders={orders}/>)}
     </div>
   )
